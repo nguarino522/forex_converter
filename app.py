38c3e4490e4122dab60b6be0859654b89140ee5b
nguarino522 <sub4miler5@gmail.com>
2022-10-12 18:02:06 -0400

    completed functionality, still need to finish testing

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 from flask import Flask, request, render_template, redirect, flash, session, jsonify
+from forex_python.converter import CurrencyRates,CurrencyCodes
+from convert import ConvertCurrency
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "24rjk24kj3"
+
+cc = ConvertCurrency()
 
 @app.route("/")
 def default_route():
     """default route"""
 
     return render_template("index.html")
+
+@app.route("/convert")
+def convert_route():
+    """convert route and handling"""
+    
+    curr_code_to = request.args.get('currencycodeto').upper()
+    curr_code_from = request.args.get('currencycodefrom').upper()
+    amount = float(request.args.get('amounttoconvert'))
+    symbol_to = cc.retrieve_currency_symbol(curr_code_to)
+    symbol_from = cc.retrieve_currency_symbol(curr_code_from)
+    
+    valid_curr_code_result = cc.check_valid_currency_code(curr_code_to, curr_code_from)
+
+    if valid_curr_code_result:
+        convert_result = cc.convert_currency(curr_code_from, curr_code_to, amount)
+        if type(convert_result) == "<class 'str'>":
+            flash(f"{convert_result}")
+            return redirect("/")
+        else:
+            flash(f"SUCCESS: Converted {symbol_from}{amount} of {curr_code_from} to {curr_code_to}. The amount is {symbol_to}{convert_result}")
+            return redirect("/")
+    elif not valid_curr_code_result:
+        flash("ERROR: One or more currency codes are invalid. Please enter a valid currency code.")
+        return redirect("/")
+    else:
+        flash("ERROR: Potential unknown error has occurred.")
+        return redirect("/")
---
+++ convert.py
@@ -0,0 +1,42 @@
+from forex_python.converter import CurrencyRates,CurrencyCodes
+
+class ConvertCurrency():
+
+    def __init__(self):
+        self.c = CurrencyRates()
+        self.cc = CurrencyCodes()
+
+    def convert_currency(self, curr_from, curr_to, curr_amount):
+        """
+        takes in 2 valid current codes and number (currency) 
+        and converts from on to the other
+        """
+        try:
+            result = self.c.convert(curr_from, curr_to, curr_amount)
+        except:
+            result = "ERROR: An exception occurred currency rate was most likely not available."
+
+        return result
+
+    
+    def check_valid_currency_code(self, curr_from, curr_to):
+        """
+        confirm if currency codes valid or not
+        as well as the number is valid
+        """
+
+        result1 = self.cc.get_currency_name(curr_from)
+        result2 = self.cc.get_currency_name(curr_to)
+
+        if result1 and result2:
+            return True
+        else:
+            return False
+            
+    
+    def retrieve_currency_symbol(self, curr_code):
+        """simply grabs specified currency code's symbol"""
+
+        symbol = self.cc.get_symbol(curr_code)
+
+        return symbol
